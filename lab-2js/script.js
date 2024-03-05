6e467893b46b8bd5d3012eadc4e72e246a05b630
Zelard9 <91178042+Zelard9@users.noreply.github.com>
2024-03-05 23:55:06 +0200

    add lab-2

--- lab-2js/script.js
+++ lab-2js/script.js
@@ -1,16 +1,31 @@
-import SpeakGoodBye from './SpeakGoodBye';
-import SpeakHello from './SpeakHello';
+(function () {
+    const names = ["Bill", "John", "Jen", "Jason", "Paul", "Frank", "Steven", "Larry", "Paula", "Laura", "Jim"];
+    let goodBye = SpeakGoodBye();
+    let hello = SpeakHello();
 
-const names = ["Bill", "John", "Jen", "Jason", "Paul", "Frank", "Steven", "Larry", "Paula", "Laura", "Jim"];
-
-for (let i = 0; i < names.length; i++) {
-    let name = names[i].toLowerCase();
-
-    if (name[0] === "j") {
-        SpeakGoodBye(name);
-    } else {
-        SpeakHello(name);
+    let lastLettersCount = {};
+    for (let i = 0; i < names.length; i++) {
+        let name = names[i];
+        let lastLetter = name[name.length - 1].toLowerCase();
+        if (lastLettersCount[lastLetter]) {
+            lastLettersCount[lastLetter]++;
+        } else {
+            lastLettersCount[lastLetter] = 1;
+        }
     }
-}
 
+    console.log("Last letter:");
+    for (let letter in lastLettersCount) {
+        console.log(letter + ": " + lastLettersCount[letter]);
+    }
 
+    for (let i = 0; i < names.length; i++) {
+        let name = names[i];
+        if (name.toLowerCase().charAt(0) === "j") {
+            goodBye(name);
+        } else {
+            hello(name);
+        }
+    }
+})();
+;
