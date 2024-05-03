2e66444e697f2c13fc9b41a2e7a408e84dd00f4f
Zelard9 <91178042+Zelard9@users.noreply.github.com>
2024-05-03 10:34:10 +0300

    update

---
+++ js/lab-3js/script.js
@@ -0,0 +1,59 @@
+const PiMultiplier = (num) => {
+    const π = Math.PI;
+    return function() {
+      return π * num;
+    };
+  };
+  
+  const multiplyByTwo = PiMultiplier(2);
+  const multiplyByThree = PiMultiplier(3);
+  const divideByTwo = PiMultiplier(0.5);
+  
+  console.log(multiplyByTwo());
+  console.log(multiplyByThree());
+  console.log(divideByTwo());
+
+
+
+const first_pi = PiMultiplier(2);
+const second_pi = PiMultiplier(2/3);
+const third_pi = PiMultiplier(0.5);
+
+console.log(first_pi());
+console.log(second_pi());
+console.log(third_pi());
+
+
+
+function Painter(color, type = undefined) {
+    if (type === undefined) {
+        console.log("No 'type' property occured!")
+        return null;
+    }
+
+    return () => { console.log(${color} - ${type}) } 
+                   
+}
+
+Painter('black')
+
+
+
+function Painter(color) { 
+    return function paint(obj) { 
+        if (obj === undefined) {
+            console.log('No \'type\' property occurred!');
+        } else {
+            console.log(${color} - ${obj.type || obj.maxSpeed});  
+        }
+      };
+}
+
+const PaintBlue = Painter('Blue'); 
+const PaintRed = Painter('Red');
+const PaintYellow = Painter('Yellow');
+console.log('Painter');
+PaintBlue({ maxSpeed: 280, type: 'Sportcar', color: 'magenta' }); 
+PaintRed({ type: 'Truck', avgSpeed: 90, loadCapacity: 2400 }); 
+PaintYellow({ maxSpeed: 180, color: 'purple', isCar: true });
+PaintYellow();  
